cb7625d4bcd513534ed71e4503cb70ffc0065650
Maria Rosu <mariarosu@tutamail.com>
2023-05-04 15:34:15 +0300

    Zoom in/out functionality

---
+++ ModEntry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ZoomAndCalendar
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            //this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+
+            if (e.Button.ToString().Equals("PageUp"))
+            {
+                if (Game1.options.desiredBaseZoomLevel < 3f)
+                    Game1.options.desiredBaseZoomLevel += 0.05f;
+            }
+            if (e.Button.ToString().Equals("PageDown"))
+            {
+                if (Game1.options.desiredBaseZoomLevel > 0.2f)
+                    Game1.options.desiredBaseZoomLevel -= 0.05f;
+            }
+        }
+    }
+}
